7572c132b522241a9e4d6175008ef64edaacf33f
vlad <sweetglory.victory@gmail.com>
2018-02-21 01:11:39 +0200

    start layout, add js for change grid layout by click

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
 import css from '../css/style.scss';
-import {initMap} from './map.js';
+//import {initMap} from './map.js';
 
 function app() {
 
-initMap();
+    var styles = getComputedStyle(document.documentElement);
+    var value = String(styles.getPropertyValue('--text-color')).trim();
+    var wrapper = document.getElementsByClassName('wrapper');
 
+    var button = document.getElementById('changeColor');
+    console.log( getComputedStyle(wrapper[0].nodeName).getPropertyValue("--layout"));
+
+    let twoColumns = '100px 1fr';
+     button.addEventListener('click', function(){
+         wrapper.documentElement.style.setProperty('--layout', twoColumns);
+     });
 
 }
 
